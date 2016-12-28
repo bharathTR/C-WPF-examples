cb1514184b96014514df80065ddb45b14ba9fa83
bharath <bluepebbles.94@gmail.com>
2016-12-28 12:37:05 +0530

    1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+            public WpfApplication3.employee emp;
+        public MainWindow()
+        {
+            InitializeComponent();
+            emp = new WpfApplication3.employee { Name = "ABC", Title = "CBX" };
+            DataContext = emp;
+           
+        }
+
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            emp.Name = "ASHA";
+            emp.Title = "SE";
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3
+{
+   public class employee : INotifyPropertyChanged
+    {
+        private string name;
+        private string title;
+        public string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                onChanged();
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+                onChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void onChanged( [CallerMemberName] string PropertyName ="" )
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this,new PropertyChangedEventArgs(PropertyName));
+        }
+
+       
+
+    }
+}
